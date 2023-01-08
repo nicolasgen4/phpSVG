162bc23a26f7e19144f730542dad1f389fcbacbb
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-01-08 14:18:50 +0100

    ajout des balises title et a
    parse les enfants et les attributs des enfants avec SimpleXMLElement dans le construct
    ajout des fonctions pour définir/retourner les balises et leurs attributs
    ajout des fonctions definirUnTtitre et definirUnLien
    ajout d'une regex à definirUneCouleur pour vérifier que c'est une couleur hexadécimale

--- src/EnteteSVG.php
+++ src/EnteteSVG.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * permet de manipuler l'entete d'un SVG et de l'afficher
  * @author David LEGRAND <wawawaformation@gmail.com>
  * @license GPL
  */
-class EnteteSVG{
+class EnteteSVG
+{
     /**
      * @var SimpleXMLElement $svg : le svg sous forme d'un arbre XML
      */
@@ -17,9 +19,7 @@ class EnteteSVG{
      * @var string $contenu : contenu à l'interieur du noeud svg
      */
     private $contenu;
-   
-   
-    
+
 
     /**
      * verifier : permet de verifier que le fichier existe et qu'il s'agit bien d'un SVG (sinon le script s'arrete)
@@ -29,18 +29,15 @@ class EnteteSVG{
      */
     private static function verifier(string $svg)
     {
-       
-        if(!file_exists($svg)){
+        if (!file_exists($svg)) {
             die('Ressource non trouvée: ' . $svg);
-            
-        } 
-        
-        if(strpos(mime_content_type($svg),'image/svg')){
-            die('Ce n\'est pas un SVG: ' . $svg );
-        
         }
-      
+        if (strpos(mime_content_type($svg), 'image/svg')) {
+            die('Ce n\'est pas un SVG: ' . $svg);
+        }
     }
+
+    //MANIPULER LES ATTRIBUTS DE LA BALISE SVG
 
     /**
      * retournerAttributs : retourne les/un attribut(s) de la balise <svg>
@@ -48,74 +45,129 @@ class EnteteSVG{
      * @param string|null $attribut : un attribut en particulier si non null
      * @return array les attributs
      */
-    private function retournerAttributs(string $attribut = null){
+    private function retournerAttributs(string $attribut = null)
+    {
         return (is_null($attribut)) ? get_object_vars($this->attributs) : $this->attributs->$attribut;
-      
-     }
-     /**
-      * definirAttribut : definir un attribut de la balise <svg> en ecrasant l'attribut si il existe deja
-      *
-      * @param string $attribut
-      * @param string $valeur
-      * @return void
-      */
-    private function definirAttribut(string $attribut, string $valeur){
+    }
+
+    /**
+     * definirAttribut : definir un attribut de la balise <svg> en ecrasant l'attribut si il existe deja
+     *
+     * @param string $attribut
+     * @param string $valeur
+     * @return void
+     */
+    private function definirAttribut(string $attribut, string $valeur)
+    {
         $this->attributs->$attribut = $valeur;
-     }
-     
+    }
+
     /**
      * supprimerAttribut : supprime un attribut de la balise <svg> si il existe
      *
      * @param string $attribut 
      * @return void
      */
-    private function supprimerAttribut(string $attribut){
-        if(!is_null($this->attributs->$attribut)) unset($this->attributs->$attribut);
+    private function supprimerAttribut(string $attribut)
+    {
+        if (!is_null($this->attributs->$attribut)) unset($this->attributs->$attribut);
     }
 
 
- 
-    
+    //MANIPULER LES BALISES A L'INTÉRIEUR DU SVG
+
+    /**
+     * definirBalise : définir une nouvelle balise dans le <svg>
+     *
+     * @param string $balise
+     * @param string $valeur
+     * @return void
+     */
+    private function definirUneBalise(string $balise, string $valeur)
+    {
+        $this->balises->$balise = $valeur;
+    }
+
+    /**
+     * retournerBalises : retourner une balise dedans le <svg>
+     *
+     * @param string|null $balise : une balise si elle n'est pas null
+     * @return array les balises
+     */
+    private function retournerBalises(string $balise = null)
+    {
+        return (is_null($balise)) ? get_object_vars($this->balises) : $this->balises->$balise;
+    }
+
+
+    //MANIPULER LES ATTRIBUTS DES AUTRES BALISES
+
+    /**
+     * definirAttributBalise : définir l'attribut d'une nouvelle balise
+     *
+     * @param string $attribut
+     * @param string $valeur
+     * @return void
+     */
+    private function definirAttributBalise(string $attribut, string $valeur = null)
+    {
+        $this->balisesAttributs->$attribut = $valeur;
+    }
+
+    /**
+     * retournerAttributBalises : retourner les attributs stockés
+     *
+     * @param string|null $attribut
+     * @return array
+     */
+    private function retournerAttributsBalises(string $attribut = null)
+    {
+        return (is_null($attribut)) ? get_object_vars($this->balisesAttributs) : $this->balisesAttributs->$attribut;
+    }
+
 
 
     /*======================================================================
                                  API
     =====================================================================*/
-    
-    
+
     /**
      * __construct : instancie la class EnteteSVG
      *
      * @param string $svg : fichier svg
      */
     public function __construct(string $svg)
-    { 
+    {
         $svg = self::retournerDirectement($svg);
-
         //les attributs de la balise svg sont stockés dans $this-attributes 
         $this->svg = new SimpleXMLElement($svg);
-        foreach($this->svg->attributes() as $clef=>$valeur){     
+        foreach ($this->svg->attributes() as $clef => $valeur) {
             $attributs[$clef] = $valeur->__toString();
         }
         $this->attributs = (object) $attributs;
 
-
-
         //On recupere le contenu du svg dans un type text (à venir : on parse avec SimpleXMLElement)
-        $svg = str_ireplace('</svg>','',$svg);
-
+        $svg = str_ireplace('</svg>', '', $svg);
         $i = 0;
-        $sous_chaine='';
-        while(true){
+        $sous_chaine = '';
+        while (true) {
             $c = $svg[$i];
             $sous_chaine .= $c;
-            if($c == '>') break;
+            if ($c == '>') break;
             $i++;
         }
-        $this->contenu = str_ireplace($sous_chaine,'',$svg);
-        
-      
-      
+        $this->contenu = str_ireplace($sous_chaine, '', $svg);
+
+        //on retourne les enfants (balises)
+        foreach ($this->svg->children() as $clef => $valeur) {
+            $balises[$clef] = $valeur->__toString();
+            //on retourner les attributs des enfants
+            foreach ($this->svg->children()->attributes() as $attribut => $donnees) {
+                $balisesAttributs[$attribut] = $donnees->__toString();
+            }
+        }
+        $this->balises = (object) $balises;
+        $this->balisesAttributs = (object) $balisesAttributs;
     }
 
 
@@ -125,22 +177,26 @@ class EnteteSVG{
      * @param string $svg : fichier svg
      * @return string $svg : le contenu du fichier svg
      */
-    public static function retournerDirectement(string $svg){
+    public static function retournerDirectement(string $svg)
+    {
         self::verifier($svg);
         return file_get_contents($svg);
     }
+
+    //MANIPULER LES ATTRIBUTS DE LA BALISE SVG
 
     /**
      * nettoyerEntete : enleve tous les attributs (sauf le viewBox) de la balise <svg>
      *
      * @return void
      */
-    public function nettoyerEntete(){
-        foreach($this->retournerAttributs() as $clef=>$valeur){
-            if($clef != 'viewBox') unset($this->attributs->$clef);    
+    public function nettoyerEntete()
+    {
+        foreach ($this->retournerAttributs() as $clef => $valeur) {
+            if ($clef != 'viewBox') unset($this->attributs->$clef);
         }
-        
     }
+
 
     /**
      * definirUneClasse : definit l'attribut classe de la balise <svg> (ecrase les classes existantes)
@@ -148,18 +204,20 @@ class EnteteSVG{
      * @param string $classe : valeur de la classe
      * @return void
      */
-    public function definirUneClasse(string $classe){
+    public function definirUneClasse(string $classe)
+    {
         $this->definirAttribut('class', $classe);
     }
 
 
     /**
-     * definirUneClasse : ajoute une classe  aux classes existantes pour la balise <svg> 
+     * ajouterUneClasse : ajoute une classe  aux classes existantes pour la balise <svg> 
      *
      * @param string $classe : valeur de la classe à ajouter
      * @return void
      */
-    public function ajouterUneClasse(string $classe){
+    public function ajouterUneClasse(string $classe)
+    {
         $this->definirAttribut('class', $this->retournerAttributs('class') . ' ' . $classe);
     }
 
@@ -170,20 +228,21 @@ class EnteteSVG{
      * @param string $id : valeur de l'id
      * @return void
      */
-    public function definirUnId(string $id){
-        $this->definirAttribut('id',$id);
+    public function definirUnId(string $id)
+    {
+        $this->definirAttribut('id', $id);
     }
 
 
     /**
      * definirUneCouleur : definit une couleur pour la balise <svg>
      *
-     * @param string $couleur : la couleur (type HTML)
+     * @param string $couleur : la couleur (type hexa)
      * @return void
      */
-    public function definirUneCouleur(string $couleur){
-        //il faudra mettre un garde-fou pour $couleur
-        $this->definirAttribut('fill', $couleur);
+    public function definirUneCouleur(string $couleur)
+    {
+        if (preg_match('/^#([0-9a-f]{3}){1,2}$/i', $couleur)) $this->definirAttribut('fill', $couleur);
     }
 
 
@@ -194,52 +253,82 @@ class EnteteSVG{
      * @param string $hauteur
      * @return void
      */
-    public function redimensionner(string $largeur, string $hauteur){
-        if(!is_null($hauteur)) $this->definirAttribut('height', $hauteur);
-        if(!is_null($hauteur)) $this->definirAttribut('width', $largeur);
+    public function redimensionner(string $largeur, string $hauteur)
+    {
+        if (!is_null($hauteur)) $this->definirAttribut('height', $hauteur);
+        if (!is_null($hauteur)) $this->definirAttribut('width', $largeur);
     }
 
 
+    //MANIPULER LES BALISES A L'INTÉRIEUR DU SVG
 
     /**
-     * definirTitle : definir un attribut title
+     * definirUnTitre : créé une balise <title>
      *
-     * @param string $title : valeur de l'attribut title
+     * @param string $titre : intitulé
      * @return void
      */
-    public function definirTitle(string $title){
-        $this->definirAttribut('title', $title);
+    public function definirUnTitre(string $titre)
+    {
+        $this->definirUneBalise('title', $titre);
     }
 
-    
+    /**
+     * definirUnLien : entoure le <svg> avec une balise <a>
+     *
+     * @param string $href
+     * @return void
+     */
+    public function definirUnLien(string $lien, string $href, string $class = null)
+    {
+        $this->definirUneBalise('a', $lien);
+        $this->definirAttributBalise('href', $href);
+        $this->definirAttributBalise('class', $class);
+    }
+
+
+    //RETOURNER LE SVG
+
     /**
      * rendre : retourne le contenu du svg
      *
      * @return void
      */
-    public function rendre(){
-        $svg = '<svg' ;
-        foreach($this->retournerAttributs() as $clef=>$valeur){
+    public function rendre()
+    {
+        if (isset($this->balises->a)) {
+            $lien = $this->retournerBalises('a');
+            $href = $this->retournerAttributsBalises('href');
+            $class = $this->retournerAttributsBalises('class');
+            $svg = '<a href="' . $href . '"><span class="' . $class .  '">' . $lien . '</span>';
+            $svg .= '<svg';
+        } else {
+            $svg = '<svg';
+        }
+        foreach ($this->retournerAttributs() as $clef => $valeur) {
             $svg .= ' ' . $clef . '="' . $valeur . '"';
         }
         $svg .= ' xmlns="http://www.w3.org/2000/svg">';
-        $svg .=$this->contenu;
-        $svg .= '</svg>';
-
+        if (isset($this->balises->title)) $svg .= '<title>' . $this->retournerBalises('title') . '</title>';
+        $svg .= $this->contenu;
+        $svg .= "\t\t" . '</svg>' . "\n";
+        if (isset($this->balises->a)) $svg .= '</a>';
         return $svg;
-       
     }
 
-    
+
     /**
      * Enregistrer : enregistre le svg dans un  fichier 
      *
      * @param string $path
      * @return void
      */
-    public function enregistrer(string $path){
-        //il faudra verifier le path (le chemin existe?, on a les droits? etc)
-        $svg = $this->rendre();
-        file_put_contents($path, $svg);
+    public function enregistrer(string $path)
+    {
+        if (is_dir($path) && is_writable($path)) {
+            $svg = $this->rendre();
+            file_put_contents($path .'/svg_' . time() . '.svg', $svg);
+            echo 'Succès : SVG enregistré';
+        }
     }
 }
